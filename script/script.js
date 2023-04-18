ea49354b5566c0301934af99bee1d81036a4afd6
Miguel Angel <angelbetancur.zapata@gmail.com>
2023-04-17 23:55:24 -0500

    pokemon generator funtion added

--- script/script.js
+++ script/script.js
@@ -1,80 +1,71 @@
-class Product{
+class Pokemon {
     constructor(id, name, xpbase, image) {
       this.id = id;
       this.name = name;
       this.xpbase = xpbase;
       this.image = image;
-    } 
+    }
   }
   
-  const products = [
-    new Product(), 
-    new Product(),
-    new Product(),
-    new Product(),
-    new Product(),
-    new Product(),
-    new Product(),
-    new Product(),
-    new Product(),
-    new Product(),
-  ];
+  const pokemonContainer = document.getElementById("cards-container");
   
-  function renderCards(productsArray) {
-    const cardsContainer = document.getElementById("cards-container");
-    cardsContainer.innerHTML = "";
-    const cards = productsArray.map((product) => `
-
-    <li>
-      <a class="card" onclick="this.classList.toggle('expanded')">
-        <img src="https://pm1.narvii.com/6720/25e80ad037ae1298d4be1d8b0b6a741c8c180347_00.jpg" class="card__image" alt="" />
-        <div class="card__overlay">
-          <div class="card__header">
-            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
-            <img class="card__thumb" src="https://pm1.narvii.com/6720/25e80ad037ae1298d4be1d8b0b6a741c8c180347_00.jpg" alt="" />
-            <div class="card__header-text">
-              <h3 class="card__title">Jessica Parker</h3>            
-              <span class="card__status">1 hour ago</span>
+  function renderCards(pokemons) {
+    const cards = pokemons.map(
+      (pokemon) => `
+        <li>
+          <a class="card" onclick="this.classList.toggle('expanded')">
+            <img src="${pokemon.image}" class="card__image" alt="${pokemon.name}" />
+            <div class="card__overlay">
+              <div class="card__header">
+                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>                     
+                <img class="card__thumb" src="${pokemon.image}" alt="${pokemon.name}" />
+                <div class="card__header-text">
+                  <h3 class="card__title">${pokemon.name}</h3>            
+                  <span class="card__status">${pokemon.xpbase} XP</span>
+                </div>
+              </div>
+              <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
             </div>
-          </div>
-          <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
-        </div>
-      </a>      
-    </li>  
-
-    `);
-    cardsContainer.insertAdjacentHTML("beforeend", cards.join(""));
+          </a>      
+        </li>  
+      `
+    );
+    pokemonContainer.innerHTML = cards.join("");
   }
   
-  renderCards(products);
-
-  const input = document.querySelector("input");
-  const button = document.querySelector("button");
-  const pokemonContainer = document.querySelector(".pokemon-container");
-
-  button.addEventListener('click', (e) => {
-    e.preventDefault();
-    traerPokemon(input.value);
-  })
-
-  function traerPokemon(pokemon) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
-    .then((res) => res.json())
-    .then((data) => {
-        crearPokemon(data);
-    });
+  async function fetchPokemons() {
+    const pokemons = [];
+    const ids = generateRandomIds(10);
+  
+    for (const id of ids) {
+      const pokemon = await fetchPokemon(id);
+      pokemons.push(pokemon);
+    }
+  
+    renderCards(pokemons);
   }
-
-  function crearPokemon(pokemon) {
-    const img = document.createElement("img");
-    img.src = pokemon.sprites.front_default;
-
-    const h3 = document.createElement("h3");
-    h3.textContent = pokemon.name;
-
-    const div = document.createElement("div");
-    div.appendChild(img);
-    div.appendChild(h3);
-
-    pokemonContainer.appendChild(div);
+  
+  async function fetchPokemon(id) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+    const data = await res.json();
+  
+    return new Pokemon(
+      data.id,
+      data.name,
+      data.base_experience,
+      data.sprites.front_default
+    );
   }
+  
+  function generateRandomIds(count) {
+    const ids = [];
+    while (ids.length < count) {
+      const id = Math.floor(Math.random() * 898) + 1;
+      if (!ids.includes(id)) {
+        ids.push(id);
+      }
+    }
+    return ids;
+  }
+  
+  fetchPokemons();
